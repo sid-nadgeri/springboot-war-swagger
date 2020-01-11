07e57c49008003b9799ad262e5444b5fb4173a8c
Siddharudh_Nadgeri <sid_nadgeri@yahoo.co.in>
2020-01-11 20:40:58 +0100

    Initial commit

---
+++ src/main/java/org/sid/springboot/demo/controller/HelloController.java
@@ -0,0 +1,14 @@
+package org.sid.springboot.demo.controller;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Hello from Springboot with War and swagger.";
+    }
+}
